178a4f7eff0aa364e2320c4fe91196d91aa239bc
Pritesh Jha <priteshjha4u@gmail.com>
2016-05-10 12:00:02 +0530

    first commit - Adding files

---
+++ public/javascripts/script.js
@@ -0,0 +1,163 @@
+
+$(document).ready(function() {
+	
+	//ajax form submission
+	$("#fileUpload").submit(function(e) {
+		e.preventDefault();
+		var frm = $(this), error = false, msg = "";
+		frm.find("input,select").each(function() {
+			if(!$(this).val()) {
+				msg += "<b>" + $(this).prev().text() + " : is a required field </b><br>";
+				error = true;
+			}
+		});
+		if(error) {
+			$("#errormsg").show().find("div").remove().end().find("button").after("<div>"+msg+"</div>");
+			setTimeout(function() {
+				if($("#errormsg").is(":visible")) {
+					$("#errormsg").fadeOut("slow");
+				}
+			}, 5000);
+			return;
+		}
+		if(!FormData) {
+			msg = "Your browser do not support HTML5.";
+			$("#errormsg").show().find("div").remove().end().find("button").after("<div>"+msg+"</div>");
+		} else {
+			$.ajax({
+				url: '/upload/uploadFile',
+				type: 'POST',
+				data: new FormData(frm[0]),
+				beforeSend: function() {
+					frm.find("input,select").each(function() {
+						$(this).attr("disabled", true);
+					});
+					$("#submitbtn").attr("disabled", true).text("Please wait...");
+				},
+				cache: false,
+				contentType: false,
+				processData: false,
+				success: function (result) {
+					if(result && result.status == "success") {
+						$("#successmsg").show().find("div").remove().end().find("button").after("<div><b>"+result.message+"</b></div>");
+						setTimeout(function() {
+							if($("#successmsg").is(":visible")) {
+								$("#successmsg").fadeOut("slow");
+							}
+						}, 5000);
+					}
+					frm.find("input,select").each(function() {
+						$(this).removeAttr("disabled");
+					});
+					$("#submitbtn").removeAttr("disabled").text("Submit");
+					frm[0].reset();
+					getFiles();
+				},
+				error: function(result) {
+					frm.find("input,select").each(function() {
+						$(this).removeAttr("disabled");
+					});
+					$("#submitbtn").removeAttr("disabled").text("Submit");
+					console.log(result);
+				}
+			});
+		}
+	});
+	
+	function getFiles() {
+		$.ajax({
+			url: '/upload/getUploadedFiles',
+			type: 'GET',
+			success: function (result) {
+				if(result && result.status == "success") {
+					var html = '<tr><td colspan="2" style="text-align:center;color:red;"><b>No Files Found</b></td></tr>';
+					if(result.data && result.data.length) {
+						$.each(result.data.split("@"), function(a,b) {
+							var f = b.split("__")[3].split(".").slice(0,2).join(".");
+							html = '<tr><td>'+f+'</td><td><button class="btn btn-primary" data-file="'+b+'">Decrypt and Download</button><button class="btn btn-danger leftsp" data-file-del="'+b+'">Delete</button></td></tr>';
+						});
+					}
+					$("#fileList").find("tbody").html(html);
+				} else {
+					alert(result.message);
+				}
+			},
+			error: function(result) {
+				console.log(result);
+			}
+		});
+	}
+	getFiles();
+	
+	$(document).on("click", "button[data-file]", function() {
+		var b = $(this), v = b.attr('data-file');
+		var html = '<form class="form-inline downloadForm" method="post" action="/upload/downloadFile"><div class="form-group"><label for="key"'+v+'>Enter Key : </label><input type="text" class="form-control" id="key"'+v+' name="key"'+v+'></div><input type="hidden" name="file" value="'+v+'"><button type="submit" class="btn btn-default leftsp">Submit</button><a href="javascript:void(0);" class="btn btn-default canceldownload leftsp" id="'+v+'">Cancel</a></form>';
+		b.next().remove().end().replaceWith(html);
+	});
+	
+	$(document).on("click", "a.canceldownload", function() {
+		var b = $(this), v = b.attr('id'), f = b.parents('form');
+		var html = '<button class="btn btn-primary" data-file="'+v+'">Decrypt and Download</button><button class="btn btn-danger leftsp" data-file-del="'+v+'">Delete</button>';
+		f.replaceWith(html);
+	});
+	
+	$(document).on("submit", "form.downloadForm", function(e) {
+		e.preventDefault();
+		if(!$(this).find("input[type=text]").val().trim()) {
+			$(this).find("input[type=text]").parent().addClass("has-error").find("input").off("focus").on("focus",function(){
+				$(this).parent().removeClass("has-error");
+			});
+		} else {
+			$(this)[0].submit();
+		}
+	});
+	
+	if(location.hash && location.hash == "#invalidKey") {
+		$("#fileList").parent().prepend('<div class="alert alert-danger" role="alert" id="invalidKey"><b>Incorrect encryption key provided. Please try again.<b></div>');
+		location.hash = "";
+		setTimeout(function() {
+			if($("#invalidKey").length) {
+				$("#invalidKey").remove();
+			}
+		}, 5000)
+	}
+	
+	$(document).on("click", "button[data-file-del]", function() {
+		var v = $(this).attr("data-file-del");
+		if(confirm('Are you sure to delete this file "' + v.split("__")[3].split(".").slice(0,2).join(".") + '"')) {
+			$.post("/upload/deleteFile", {filename:v} ).done(function(result) {
+				if(result && result.status == "success") {
+					$("#fileList").parent().prepend('<div class="alert alert-success" role="alert" id="smG"><b>File deleted successfully<b></div>');
+					setTimeout(function() {
+						if($("#smG").is(":visible")) {
+							$("#smG").remove();
+						}
+					}, 5000);
+					getFiles();
+				} else {
+					console.log(result);
+				}
+			}).fail(function(result) {
+				console.log(result);
+			});
+		}
+	});
+	
+	function getCiphers() {
+		$.get("/upload/getCiphers").done(function(d){
+			if(d && d.status == "success") {
+				var html = "";
+				if(d.data.split(",").length) {
+					$.each(d.data.split(","), function(a,b) {
+						html += '<option value="'+b+'">'+b+'</option>';
+					});
+					$('#ealgo').append(html);
+				}
+			}
+			
+		}).fail(function(r) {
+			console.log(r);
+		});
+	}
+	getCiphers();
+});
---
+++ routes/upload.js
@@ -0,0 +1,121 @@
+var express = require('express');
+var router = express.Router();
+
+var multer  = require('multer');
+var upload = multer({ dest: 'uploads/tmp/'});
+var fs = require('fs'), crypto = require('crypto');
+
+router.get('/', function(req, res, next) {
+  res.json({
+	status : "error",
+	message : "Invalid Request"
+  });
+});
+
+router.get('/getCiphers', function(req, res, next) {
+  var list = crypto.getCiphers();
+  res.json({status:"success",data: list.length ? list.join() : ""});
+});
+
+router.post('/', function(req, res, next) {
+  res.json({
+	status : "error",
+	message : "Invalid Request"
+  });
+});
+
+router.get('/getUploadedFiles', function(req, res, next) {
+	fs.readdir("uploads/", function(err, files) {
+		if(err) {
+			console.log("Error:", err);
+			res.json({status:"error",message:"Something went wrong."});
+			return false;
+		}
+		files.splice(files.indexOf("tmp"), 1);
+		res.json({status:"success", data: files.length ? files.join("@") : ""});
+	});
+});
+
+router.post('/uploadFile', upload.single('inputFile'), function(req, res, next) {
+	if(!req.file) {
+		res.json({status:"error",message:"Please upload a file."});
+		return false;
+	}
+	var tmp_path = req.file.path, fname = req.file.originalname, key = req.body.ekey || null;
+	if(!key) {
+		fs.unlinkSync(tmp_path);
+		res.json({status:"error",message:"Encryption key is required."});
+		return false;
+	}
+	var _key = new Buffer(key).toString('base64');
+	var target_path = 'uploads/' + "__" + _key + "__" + Date.now() + "__" + fname + ".enc";
+	var cipher = crypto.createCipher('aes-256-cbc', key);
+	var src = fs.createReadStream(tmp_path);
+	var dest = fs.createWriteStream(target_path);
+	src.pipe(cipher).pipe(dest);
+	dest.on("error", function(err) {
+		console.log(err);
+		res.json({status:"error",message:"Something went wrong."});
+	});
+	dest.on("finish", function() {
+		fs.unlinkSync(tmp_path);
+		res.json({
+			status : "success",
+			message : "File uploaded and encrypted successfully"
+		});
+	});
+});
+
+router.post('/downloadFile', function(req, res, next) {
+	var key = req.body.key || null;
+	var file = req.body.file || null;
+	if(!key || !file) {
+		res.redirect('/');
+		return false;
+	}
+	var _key = file.split("__")[1];
+	if(new Buffer(_key, 'base64').toString('ascii') != key) {
+		res.redirect('/#invalidKey');
+		return false;
+	}
+	var _file = "uploads/"+file;
+	var target_path = 'uploads/' + file.split("__")[3].split(".").slice(0,2).join(".");
+	var decipher = crypto.createDecipher('aes-256-cbc', key);
+	var src = fs.createReadStream(_file);
+	var dest = fs.createWriteStream(target_path);
+	src.pipe(decipher).pipe(dest);
+	dest.on("error", function(err) {
+		console.log(err);
+		res.end("<h1>Something went wrong</h1><br><a href='/'>Go Back</a>");
+		return false;
+	});
+	dest.on("finish", function() {
+		res.download(target_path, file.split("__")[3].split(".").slice(0,2).join("."), function(err){
+			if (err) {
+			console.log(err);
+			} else {
+				fs.unlinkSync(target_path);
+			}
+		});
+	});
+});
+
+router.post('/deleteFile', function(req, res, next) {
+	var file = req.body.filename || null;
+	if(file) {
+		fs.unlink('uploads/'+file, function(err) {
+			if(err) {
+				console.log(err);
+				res.json({status:"error", message: "Something went wrong"});
+			} else {
+				res.json({status:"success"});
+			}
+		});
+	} else {
+		res.json({status:"error", message: "Invalid file"});
+	}
+});
+
+
+
+module.exports = router;
